a50c2c88c49cd513bb020df1de34eb89a959cb62
Wilson Wong <williewonga@gmail.com>
2016-09-25 16:51:22 -0500

    Add files via upload

---
+++ drawsun.py
@@ -0,0 +1,23 @@
+import turtle
+
+def draw_sun():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    pen = turtle.Turtle()
+
+    pen.shape("classic")
+    pen.color("yellow")
+    pen.speed(10)
+    squares = 0
+    while squares < 20:
+        count = 0
+        while count < 4:
+            pen.forward(100)
+            pen.right(90)
+            count += 1
+        pen.right(18)
+        squares += 1
+    window.exitonclick()
+
+draw_sun()
