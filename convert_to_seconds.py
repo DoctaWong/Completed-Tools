c313720c506af8a03c807e0f35afcc97ca875105
Wilson Wong <williewonga@gmail.com>
2016-09-26 03:21:43 -0500

    Update convert_to_seconds.py

--- convert_to_seconds.py
+++ convert_to_seconds.py
@@ -9,11 +9,11 @@ Created on Sun Sep 25 16:00:39 2016
 years = int(input("How many years?"))
 days = int(input("How many days?"))
 hours = int(input("How many hours?"))
-minutes = int(input("Minutes?"))
-seconds = int(input("Seconds?"))
+minutes = int(input("How many minutes?"))
+seconds = int(input("How many seconds?"))
 
-def total_seconds(hours,minutes,seconds):
+def total_seconds(years, days, hours, minutes, seconds):
     total_seconds = (years * 24 *365 *3600) + (days * 24*3600) + (hours * 3600) + (minutes * 60) + seconds
     return total_seconds
 
-print ("There are %s seconds in %s years, %s days, %s hours, %s minutes, and %s seconds" % (total_seconds(years, days, hours, minutes, seconds), hours, minutes, seconds))
+print ("There are %s seconds in %s years, %s days, %s hours, %s minutes, and %s seconds" % (total_seconds(years, days, hours, minutes, seconds), years, days, hours, minutes, seconds))
