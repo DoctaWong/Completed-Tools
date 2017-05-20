31e55d5932bfbdd28015a5dd06c2d6f8dc52ae0d
Wilson Wong <williewonga@gmail.com>
2017-05-20 10:32:59 -0500

    Update OOP_drawing.py

--- OOP_drawing.py
+++ OOP_drawing.py
@@ -1,4 +1,5 @@
 import turtle
+import winsound
 
 def draw_sun():
     window = turtle.Screen()
@@ -10,14 +11,19 @@ def draw_sun():
     pen.color("yellow")
     pen.speed(10)
     squares = 0
-    while squares < 20:
+    
+    while squares < 20: 
+
         count = 0
+        soundfile = "pensound.wav"
+        winsound.PlaySound(soundfile, winsound.SND_ASYNC)
         while count < 4:
             pen.forward(100)
             pen.right(90)
             count += 1
         pen.right(18)
         squares += 1
+    winsound.PlaySound(None, winsound.SND_ALIAS)
     window.exitonclick()
 
 draw_sun()
