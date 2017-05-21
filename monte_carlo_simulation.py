4a7c447e6d89a48c5310bee485d6ace212b0b338
Wilson Wong <williewonga@gmail.com>
2017-05-21 16:15:12 -0500

    Update monte_carlo_simulation.py

--- monte_carlo_simulation.py
+++ monte_carlo_simulation.py
@@ -3,7 +3,8 @@ def drawing_without_replacement_sim(numTrials):
     Runs numTrials trials of a Monte Carlo simulation
     of drawing 3 balls out of a bucket containing
     4 red and 4 green balls. Balls are not replaced once
-    drawn. Returns a float - the fraction of times 3 
+    drawn. 
+    Returns a float - the fraction of times 3 
     balls of the same color were drawn in the first 3 draws.
     '''
     sum = 0
