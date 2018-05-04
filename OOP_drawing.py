86409615d6add6accc965cd3dd0cf9f95ae5a483
Wilson Wong <williewonga@gmail.com>
2018-05-04 16:26:23 -0500

    Update OOP_drawing.py

--- OOP_drawing.py
+++ OOP_drawing.py
@@ -13,17 +13,13 @@ def draw_sun():
     squares = 0
     
     while squares < 20: 
-
         count = 0
-        soundfile = "pensound.wav"
-        winsound.PlaySound(soundfile, winsound.SND_ASYNC)
         while count < 4:
             pen.forward(100)
             pen.right(90)
             count += 1
         pen.right(18)
         squares += 1
-    winsound.PlaySound(None, winsound.SND_ALIAS)
     window.exitonclick()
 
 draw_sun()
